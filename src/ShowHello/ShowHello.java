794aec5d5d3086e005cf1cec61a2df57cea9b7be
Mon0106 <dongnam.japan@gmail.com>
2020-07-03 13:43:05 +0700

    Hiển thị lời chào

---
+++ src/ShowHello/ShowHello.java
@@ -0,0 +1,14 @@
+package ShowHello;
+
+import java.util.Scanner;
+
+public class ShowHello {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name:");
+        String name = scanner.nextLine();
+
+        System.out.print("Hello: " + name);
+    }
+}
